003946f390432c212652c5af04ae75392b8781dc
Elias Cando <ecando@corporacionlanec.com>
2025-06-24 22:49:44 -0500

    login start

---
+++ lib/views/register.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Registro')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Contraseña'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica de registro aquí
+                print('Email: ${emailController.text}');
+                print('Contraseña: ${passwordController.text}');
+              },
+              child: Text('Registrarse'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
